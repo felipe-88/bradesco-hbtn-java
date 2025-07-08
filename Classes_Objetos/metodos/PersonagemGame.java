3e421ec762ce6713669aad5b479addce5a0ab74f
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-07-08 14:35:18 -0300

    Task 2 Métodos

--- Classes_Objetos/metodos/PersonagemGame.java
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -17,4 +17,12 @@ public class PersonagemGame {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void tomarDano(int quantidadeDeDano) {
+        saudeAtual = Math.max(saudeAtual - quantidadeDeDano, 0);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        saudeAtual = Math.min(saudeAtual + quantidadeDeCura, 100);
+    }
 }
