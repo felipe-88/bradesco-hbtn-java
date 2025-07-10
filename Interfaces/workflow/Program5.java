c4a3bcb15f815b576e57743b2dc4e94ab16845c3
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-07-10 11:37:01 -0300

    Task 0 Workflow

--- Interfaces/workflow/Program5.java
+++ Interfaces/workflow/Program5.java
@@ -1,4 +1,4 @@
-public class Program {
+public class Program5 {
     public static void main(String[] args) {
         ProcessadorVideo processadorVideo1 = new ProcessadorVideo();
         processadorVideo1.registrarCanal(new Slack());
