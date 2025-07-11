38e0b75a2f6c79ce17c3bb0864cf945fd25cf209
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-07-11 15:32:30 -0300

    Task 1 Leitura de Arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+
+
+        // efetue a leitura do arquivo
+        System.out.println("Conteúdo do arquivo 'exemplo.txt':\n");
+        try(BufferedReader reader = new BufferedReader(new FileReader("arquivos/1/"+fileName))) {
+            reader.lines().forEach(System.out::println);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("\nLeitura do arquivo concluída.");
+    }
+}
