46607319158ac830e6834443faf4390e771d3de9
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-09-02 18:21:09 -0300

    0. Pessoas agrupadas por cargo

---
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %f", this.codigo, this.nome, this.cargo, this.idade, this.salario);
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+}
