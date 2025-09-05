db534ca7ef40b6a1dd7fff15b4e6792bfe90d2d6
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-09-05 19:29:48 -0300

    0. Sincronização com Threads - correção

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
         ThreadContador t1 = new ThreadContador(contador);
         ThreadContador t2 = new ThreadContador(contador);
         t1.start();
-        t2.start();
         t1.run();
+        t2.start();
         t2.run();
         t1.interrupt();
         t2.interrupt();
