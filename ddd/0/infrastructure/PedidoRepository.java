1773a0ddf1092c13868a10369b7d3a94b131cf59
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-10-06 15:03:49 -0300

    0. Implementando com DDD

---
+++ ddd/0/infrastructure/PedidoRepository.java
@@ -0,0 +1,24 @@
+package infrastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import domain.Pedido;
+
+
+public class PedidoRepository {
+
+
+    private final Map<String, Pedido> pedidos = new HashMap<>();
+
+
+    public void salvar(Pedido pedido) {
+        this.pedidos.merge(pedido.getId(), pedido, (pedidoOld,pedidoNew) -> pedidoNew);
+    }
+
+
+    public Pedido buscarPorId(String id) {
+        return this.pedidos.get(id);
+    }
+}
